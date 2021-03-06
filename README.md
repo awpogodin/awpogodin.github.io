08eaaf15aa134d39d99720003fd044faf24379da
Alexandr Pogodin <awpogodin@mail.ru>
2021-03-06 14:59:10 +0400

    00000 Init

---
+++ README.md
@@ -0,0 +1,27 @@
+# awpogodin page
+
+[![Dependabot badge](https://flat.badgen.net/dependabot/wbkd/webpack-starter?icon=dependabot)](https://dependabot.com/)
+
+### Installation
+
+```
+npm install
+```
+
+### Start Dev Server
+
+```
+npm start
+```
+
+### Build Prod Version
+
+```
+npm run build
+```
+
+### Deploy to GitHub Pages
+
+```
+npm run deploy
+```
---
+++ src/scripts/index.js
@@ -0,0 +1,38 @@
+import "../styles/index.scss";
+
+if (process.env.NODE_ENV === "development") {
+  require("../index.html");
+}
+
+const THEME_KEY = "light_mode";
+
+const Storage = {
+  getItem: (key) => localStorage.getItem(key),
+  getBooleanItem: (key) =>
+    localStorage.getItem(key) === "true" ? true : false,
+  setItem: (key, value) => localStorage.setItem(key, value),
+};
+
+const getBtnIcon = (day) => day ? "☀️" : "🌒";
+
+const init = () => {
+  const btn = document.querySelector("#switch-theme");
+  const elements = document.querySelectorAll("[th]");
+
+  let isLightMode = false;
+
+  const changeTheme = () => {
+    elements.forEach((el) => {
+      el.classList.toggle("themed");
+    });
+    isLightMode = !isLightMode;
+    btn.innerHTML = getBtnIcon(isLightMode);
+    localStorage.setItem(THEME_KEY, isLightMode);
+  };
+
+  Storage.getBooleanItem(THEME_KEY) && changeTheme(); // dark mode is default
+  btn.innerHTML = getBtnIcon(isLightMode);
+  btn.addEventListener("click", changeTheme);
+};
+
+init();
